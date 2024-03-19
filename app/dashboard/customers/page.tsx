f97e284262ad3ab448d89986e2d1d4f5c96f5bb8
danaharley <danaharley.github@gmail.com>
2024-03-19 20:00:19 +0700

    completed

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+const CustomersPage = () => {
+  return <div>Customers Page</div>;
+};
+
+export default CustomersPage;
